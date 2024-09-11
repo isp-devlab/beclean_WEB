a217e708c6f3f5b927a7ebea99d0192435d125a4
fajar-dev <fajarrivaldi2015@gmail.com>
2024-09-11 15:03:47 +0700

    fix: fixed sensitive case

--- app/Models/product.php
+++ app/Models/product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class product extends Model
+class Product extends Model
 {
     use HasFactory;
     public $timestamps = true;
--- app/Models/productCategory.php
+++ app/Models/productCategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class productCategory extends Model
+class ProductCategory extends Model
 {
     use HasFactory;
     public $timestamps = true;
